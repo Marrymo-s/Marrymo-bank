f6840f121b4393d15664acbbf64b859c5956475d
박도연 <ehduszkdzkd@naver.com>
2024-03-12 15:21:51 +0900

    feat : Request Dto에 생성자 추가

--- back/marrymo/src/main/java/site/marrymo/restapi/user/dto/request/InvitationIssueRequest.java
+++ back/marrymo/src/main/java/site/marrymo/restapi/user/dto/request/InvitationIssueRequest.java
@@ -1,11 +1,13 @@
 package site.marrymo.restapi.user.dto.request;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class InvitationIssueRequest {
     @NotNull(message = "청첩장 발급 여부 정보는 null 값이면 안됩니다")
     private Boolean isIssued;
