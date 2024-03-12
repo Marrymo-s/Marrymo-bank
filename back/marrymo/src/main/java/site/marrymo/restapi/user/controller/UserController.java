a7bea78a23c609cbfa8665a6348783eff2b33a40
박도연 <ehduszkdzkd@naver.com>
2024-03-12 13:36:02 +0900

    feat: controller 회원 정보 조회 구현

--- back/marrymo/src/main/java/site/marrymo/restapi/user/controller/UserController.java
+++ back/marrymo/src/main/java/site/marrymo/restapi/user/controller/UserController.java
@@ -3,12 +3,11 @@ package site.marrymo.restapi.user.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import site.marrymo.restapi.user.dto.request.UserModifyRequest;
 import site.marrymo.restapi.user.dto.request.UserRegistRequest;
+import site.marrymo.restapi.user.dto.response.UserGetResponse;
 import site.marrymo.restapi.user.service.UserService;
 
 @Slf4j
@@ -25,5 +24,11 @@ public class UserController {
     @PutMapping
     public void modifyUserInfo(@Valid UserModifyRequest userModifyRequest){
         userService.modifyUserInfo(1L, userModifyRequest);
+    }
+
+    @GetMapping
+    public ResponseEntity<UserGetResponse> getUserInfo(){
+        UserGetResponse userGetResponse = userService.getUserInfo(1L);
+        return ResponseEntity.ok(userGetResponse);
     }
 }
