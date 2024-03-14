2a85c1be91055cbaaa63d97960f97e38439a7f10
박도연 <ehduszkdzkd@naver.com>
2024-03-14 14:18:26 +0900

    feat: moneyList 조회 구현

--- back/marrymo/src/main/java/site/marrymo/restapi/moneygift_history/repository/MoneygiftRepository.java
+++ back/marrymo/src/main/java/site/marrymo/restapi/moneygift_history/repository/MoneygiftRepository.java
@@ -3,11 +3,9 @@ package site.marrymo.restapi.moneygift_history.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.marrymo.restapi.moneygift_history.entity.Moneygift;
 import site.marrymo.restapi.user.entity.User;
-import site.marrymo.restapi.wishitem.entity.WishItem;
 
 import java.util.List;
-import java.util.Optional;
 
-public class MoneygiftRepository {
-
+public interface MoneygiftRepository extends JpaRepository<Moneygift, Long> {
+    List<Moneygift> findByUser(User user);
 }
--- back/marrymo/src/main/java/site/marrymo/restapi/user/exception/UserErrorCode.java
+++ back/marrymo/src/main/java/site/marrymo/restapi/user/exception/UserErrorCode.java
@@ -8,7 +8,7 @@ import site.marrymo.restapi.global.exception.ErrorCode;
 @AllArgsConstructor
 public enum UserErrorCode implements ErrorCode {
     USER_NOT_FOUND(404, "USE_001", "찾을 수 없는 회원"),
-    USER_ALREADY_DELETE(404, "USE_006", "탈퇴 중 에러 발생");
+    USER_ALREADY_DELETE(404, "USE_006", "이미 탈퇴한 회원");
 
     private int statusCode;
     private String errorCode;
