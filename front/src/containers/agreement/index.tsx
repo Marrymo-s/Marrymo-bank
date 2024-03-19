7e239fef064bb1286f2235ea164fd1c950a51339
김하연 <hayeo3496@gmail.com>
2024-03-19 11:00:51 +0900

    docs : delete front folder

--- front/src/containers/agreement/index.tsx
+++
@@ -1,9 +0,0 @@
-import Header from '@/components/Header'
-
-const Agreement = () => {
-  return (
-    <Header title='개인 정보 수집 동의' hasPrevious/>
-  )
-}
-
-export default Agreement;
--- front/src/containers/letterwrite/index.tsx
+++
@@ -1,7 +0,0 @@
-const LetterWrite =() => {
-  return (
-    <div>롤링페이퍼 작성페이지</div>
-  )
-}
-
-export default LetterWrite;
