698369814872af9aa956d8552b4238ce315fac8a
정지원 <imjjw1004@naver.com>
2024-03-19 17:00:16 +0900

    feat : smtp를 위한 이메일 ㅇ인증로직 구현

--- back/marrymo/src/main/java/site/marrymo/restapi/auth/controller/AuthController.java
+++ back/marrymo/src/main/java/site/marrymo/restapi/auth/controller/AuthController.java
@@ -23,7 +23,7 @@ public class AuthController {
     public void registMail(@Valid @RequestBody SmtpRegistRequest smtpRegistRequest) {
         smtpService.registMail(smtpRegistRequest.getEmail(), "Marrymo 이메일 인증", "이메일 인증 번호 : ");
     }
-//
+
 //    @PostMapping("/verify-email")
 //    public ResponseEntity verifyMail(@Valid @RequestBody SmtpVerifyRequest smtpVerifyRequest) {
 //        return true;
--- back/marrymo/src/main/java/site/marrymo/restapi/auth/service/AuthService.java
+++ back/marrymo/src/main/java/site/marrymo/restapi/auth/service/AuthService.java
@@ -1,5 +1,15 @@
 package site.marrymo.restapi.auth.service;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class AuthService {
     //이메일 인증 로직
+
 }
