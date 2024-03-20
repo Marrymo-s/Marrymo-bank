89b14495efcbd7532ebf8f575d8108cbeae3cd60
김재윤 <jaeyun1723@naver.com>
2024-03-20 16:41:08 +0900

    test : /token log추가

--- back/bank/src/main/java/site/bank/restapi/auth/controller/AuthController.java
+++ back/bank/src/main/java/site/bank/restapi/auth/controller/AuthController.java
@@ -1,11 +1,13 @@
 package site.bank.restapi.auth.controller;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import site.bank.restapi.auth.dto.common.TokenDto;
@@ -20,9 +22,11 @@ public class AuthController {
 
 	private final AuthService authService;
 	@PostMapping("/token")
-	public ResponseEntity<?> authenticateClient(@RequestBody ClientRequest client){
+	public ResponseEntity<?> authenticateClient(@RequestBody ClientRequest client, HttpServletRequest request){
 		log.debug("Authenticate Start...");
+		log.debug("request ip = {} ",request.getRemoteAddr());
 		TokenDto token= authService.authenticateClient(client);
+		log.debug("Client is authenticated...");
 		return ResponseEntity.ok(token);
 	}
 }
