44f770ca48a5880c876c3fecdf8ca76de5d78d17
김재윤 <jaeyun1723@naver.com>
2024-03-20 17:24:42 +0900

    hotfix : env

--- back/bank/src/main/java/site/bank/restapi/auth/mapper/AuthMapper.java
+++ back/bank/src/main/java/site/bank/restapi/auth/mapper/AuthMapper.java
@@ -6,19 +6,18 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import site.bank.restapi.auth.dto.common.ClientDto;
 import site.bank.restapi.auth.exception.AuthErrorCode;
 import site.bank.restapi.auth.exception.AuthException;
 
 @Repository
 @PropertySource("classpath:application.yml")
+@AllArgsConstructor
 public class AuthMapper {
 
 	private Environment env;
-	@Autowired
-	public AuthMapper(Environment env){
-		this.env=env;
-	}
 
 	public ClientDto findByClientId(String clientId) {
 		if (clientId.equals(env.getProperty("institution.client_id")))
