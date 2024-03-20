aa5570e594ea76e4e20f9a3a9e70fc2f3947a0a3
김하연 <khy3496@cau.ac.kr>
2024-03-20 14:01:08 +0900

    feat: SpringBootApplication exclude Auto 제거

--- back/bank/src/main/java/site/bank/restapi/BankApplication.java
+++ back/bank/src/main/java/site/bank/restapi/BankApplication.java
@@ -3,7 +3,7 @@ package site.bank.restapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class BankApplication {
 
     public static void main(String[] args) {
