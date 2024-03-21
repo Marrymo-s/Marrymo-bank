23fb5142a036190b1c7a3501ab16e5d6df4ade5b
seunggwang <sg.ryu23@gmail.com>
2024-03-21 11:19:53 +0900

    feat: Wrapper 적용 작업 완료

--- front/src/containers/landing/index.tsx
+++ front/src/containers/landing/index.tsx
@@ -8,17 +8,18 @@ import Carousel from '@/containers/landing/carousel';
 
 const Landing = () => {
   return (
-    <main className={styles.landingWrapper}>
-      {/*<Heading />*/}
-      <div className={styles.landingText({ contentType: 'title' })}>
-        Marrymo
-      </div>
-      <div className={styles.landingText({ contentType: 'content' })}>
-        우리만의 특별한 결혼식 만들기
-      </div>
-      <Carousel />
-      <KakaoLoginButton />
-    </main>
+    <>
+      <main className={styles.landingWrapper}>
+        <div className={styles.landingText({contentType: 'title'})}>
+          Marrymo
+        </div>
+        <div className={styles.landingText({contentType: 'content'})}>
+          우리만의 특별한 결혼식 만들기
+        </div>
+        <Carousel/>
+        <KakaoLoginButton/>
+      </main>
+    </>
   )
 }
 
--- front/src/containers/letterwrite/index.tsx
+++ front/src/containers/letterwrite/index.tsx
@@ -1,6 +1,6 @@
-const LetterWrite =() => {
+const LetterWrite = () => {
   return (
-    <div>롤링페이퍼 작성페이지</div>
+    <div>롤링페이퍼 작성페이지(임시 폐쇄)</div>
   )
 }
 
