4db3c8e36635cde8e64d42bbfc7431828e73516d
김재윤 <jaeyun1723@naver.com>
2024-03-22 15:25:16 +0900

    feat : cardService

--- back/marrymo/src/main/java/site/marrymo/restapi/card/repository/CardRepository.java
+++ back/marrymo/src/main/java/site/marrymo/restapi/card/repository/CardRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import site.marrymo.restapi.card.entity.Card;
 import site.marrymo.restapi.user.entity.User;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface CardRepository extends JpaRepository<Card, Long> {
     Optional<Card> findByUser(User user);
+
+    Optional<List<Card>> findAllByWeddingDate(LocalDate localDate);
 }
--- back/marrymo/src/main/java/site/marrymo/restapi/card/service/CardService.java
+++ back/marrymo/src/main/java/site/marrymo/restapi/card/service/CardService.java
@@ -1,14 +1,26 @@
 package site.marrymo.restapi.card.service;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import site.marrymo.restapi.card.entity.Card;
+import site.marrymo.restapi.card.repository.CardRepository;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class CardService {
+
+	private final CardRepository cardRepository;
+
+	public List<Card> findAllByWeddingDate(LocalDate localDate){
+
+	}
 }
