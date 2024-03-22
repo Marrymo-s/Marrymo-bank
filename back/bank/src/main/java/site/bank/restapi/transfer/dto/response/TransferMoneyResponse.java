e6395f2bc033f327cf3cc12dd1f0063702fecd40
김하연 <khy3496@cau.ac.kr>
2024-03-22 10:13:18 +0900

    fix : TransferMoneyRequest, Response fintechUseNum 삭제

--- back/bank/src/main/java/site/bank/restapi/transfer/dto/response/TransferMoneyResponse.java
+++ back/bank/src/main/java/site/bank/restapi/transfer/dto/response/TransferMoneyResponse.java
@@ -11,7 +11,6 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TransferMoneyResponse {
-    String fintechUseNum;
     int tranAmt;
     String senderName;
     String receiverName;
