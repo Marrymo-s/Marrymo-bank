a68f6f1bdca5397214699ad71425010b7f3c0628
정지원 <imjjw1004@naver.com>
2024-03-22 15:40:58 +0900

    feat : 축의금 정산내역 í 페이지 로직 구현

--- front/src/containers/history/HistoryList.tsx
+++ front/src/containers/history/HistoryList.tsx
@@ -3,15 +3,17 @@
 import React, { useState } from 'react'
 import {historyListContainer} from "@/containers/history/index.css";
 import axios from "axios";
-
+import {historyResponse} from "@/types/history";
+import {moneygifts} from "@/types/history";
 
 const HistoryList = () => {
-    // const [historyData, sethistoryData] = useState<HistoryData[]
+    const [historyData, setHistoryData] = useState<moneygifts[]>()
 
     const getHistory = async () => {
         try {
-            const response = await axios.get('/moneygift')
-
+            const response = await axios.get<historyResponse>('/moneygift')
+            console.log(response);
+            // setHistoryData(response.data.items.moneyList); //서버 정상화되면 연결해서 테스트해볼 예정
         } catch(error) {
             console.error('moneygift history get중 오류 발생', error)
         }
@@ -20,7 +22,28 @@ const HistoryList = () => {
     return (
         <>
             <div className={historyListContainer}>
-                아리소
+                <table>
+                    <thead>
+                    <tr>
+                        <th>이름</th>
+                        <th>금액</th>
+                        <th>유형</th>
+                        <th>하객</th>
+                        <th>비고</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {historyData?.map((history,  index) => (
+                        <tr key={index}>
+                            <td>{history.sender}</td>
+                            <td>{history.amount}</td>
+                            <td>{history.type}</td>
+                            <td>{history.guestType}</td>
+                            <td>{history.relationship}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
 
             </div>
         </>
