ce64956f67c21d401d9864a449fe119ca629beca
김하연 <khy3496@cau.ac.kr>
2024-03-25 13:51:56 +0900

    hotfix : RefreshTokenRepository @EnableRedisRepositories annotation 추가

--- back/marrymo/src/main/java/site/marrymo/restapi/global/jwt/repository/RefreshTokenRepository.java
+++ back/marrymo/src/main/java/site/marrymo/restapi/global/jwt/repository/RefreshTokenRepository.java
@@ -1,7 +1,8 @@
 package site.marrymo.restapi.global.jwt.repository;
 
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.repository.CrudRepository;
 import site.marrymo.restapi.global.jwt.entity.RefreshToken;
-
+@EnableRedisRepositories
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
 }
