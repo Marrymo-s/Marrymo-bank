80336271f9e87fb00039f1756ccb2718f0388099
김재윤 <jaeyun1723@naver.com>
2024-03-25 17:53:28 +0900

    feat : webSecurity

--- back/marrymo/src/main/java/site/marrymo/restapi/global/auth/security/config/SecurityConfig.java
+++ back/marrymo/src/main/java/site/marrymo/restapi/global/auth/security/config/SecurityConfig.java
@@ -2,13 +2,17 @@ package site.marrymo.restapi.global.auth.security.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+
 import site.marrymo.restapi.global.auth.security.handler.OAuth2LoginSuccessHandler;
 import site.marrymo.restapi.global.auth.security.service.CustomOAuth2UserService;
 
@@ -17,25 +21,32 @@ import site.marrymo.restapi.global.auth.security.service.CustomOAuth2UserService
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private final CustomOAuth2UserService customOAuth2UserService;
-    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+	private final CustomOAuth2UserService customOAuth2UserService;
+	private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement((sessionManagement) ->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeRequests()
-                .requestMatchers("/error").permitAll()
-                .requestMatchers("/api/", "/graphiql", "/graphql",
-                        "/swagger-ui/", "/api-docs", "/swagger-ui.html",
-                        "/v3/api-docs/", "/api-docs/", "/swagger-ui.html").permitAll()
-                .anyRequest().permitAll()
-                .and()
-                .oauth2Login((oauth2) -> oauth2
-                        .successHandler(oAuth2LoginSuccessHandler)
-                        .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
-                                .userService(customOAuth2UserService)));
-        return http.build();
-    }
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return web -> web.ignoring()
+			.requestMatchers(PathRequest.toStaticResources().atCommonLocations()) // 정적 리소스들
+			.requestMatchers("/swagger-ui/**", "/v3/api-docs/**");
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf(AbstractHttpConfigurer::disable)
+			.sessionManagement((sessionManagement) ->
+				sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.authorizeRequests()
+			.requestMatchers("/error").permitAll()
+			.requestMatchers("/api/", "/graphiql", "/graphql",
+				"/swagger-ui/", "/api-docs", "/swagger-ui.html",
+				"/v3/api-docs/", "/api-docs/", "/swagger-ui.html").permitAll()
+			.anyRequest().permitAll()
+			.and()
+			.oauth2Login((oauth2) -> oauth2
+				.successHandler(oAuth2LoginSuccessHandler)
+				.userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
+					.userService(customOAuth2UserService)));
+		return http.build();
+	}
 }
