1ce8eca1c30cfb2de5704eff3aeb2d591aafd825
김재윤 <jaeyun1723@naver.com>
2024-03-26 14:28:48 +0900

    fix:jwtFilter

--- back/marrymo/src/main/java/site/marrymo/restapi/global/jwt/filter/JwtAuthenticationFilter.java
+++ back/marrymo/src/main/java/site/marrymo/restapi/global/jwt/filter/JwtAuthenticationFilter.java
@@ -9,8 +9,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
+
 import site.marrymo.restapi.global.exception.UnAuthorizedException;
 import site.marrymo.restapi.global.jwt.JWTProvider;
 import site.marrymo.restapi.global.jwt.dto.TokenDTO;
@@ -24,143 +26,148 @@ import java.util.Map;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends GenericFilterBean {
-    private final JWTProvider jwtProvider;
-    private final RefreshTokenRepository refreshTokenRepository;
+	private final JWTProvider jwtProvider;
+	private final RefreshTokenRepository refreshTokenRepository;
 
-    /**
-     * [요청 시 거치는 필터 로직]
-     * Request는 아래와 같은 로직을 통과한다
-     * 1. Request에서 쿠키를 가져온 후 accessToken과 refreshToken을 추출한다. (token을 토대로 userCode도 가져온다)
-     * 2. jwtProvider는 유효하지 않은 토큰이 있다면 다시 생성한 후 map에 담아서 가져온다.
-     * (ex) accessToken만이 유효하지 않았다면 accessToken만 map에 담아서 가져온다.
-     * 3. accessToken이 유효하지 않았다면 기존에 accessToken을 담고 있던 쿠키를 삭제 시키고 다시 발급한 accessToken을 쿠키에 담는다.
-     * 4. HttpServletResponse에 cookie를 담아서 보낸다.
-     *
-     * @param servletRequest
-     * @param servletResponse
-     * @param filterChain
-     * @throws IOException
-     * @throws ServletException
-     */
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
-        log.debug("reguestURI = {}",httpServletRequest.getServletPath());
+	/**
+	 * [요청 시 거치는 필터 로직]
+	 * Request는 아래와 같은 로직을 통과한다
+	 * 1. Request에서 쿠키를 가져온 후 accessToken과 refreshToken을 추출한다. (token을 토대로 userCode도 가져온다)
+	 * 2. jwtProvider는 유효하지 않은 토큰이 있다면 다시 생성한 후 map에 담아서 가져온다.
+	 * (ex) accessToken만이 유효하지 않았다면 accessToken만 map에 담아서 가져온다.
+	 * 3. accessToken이 유효하지 않았다면 기존에 accessToken을 담고 있던 쿠키를 삭제 시키고 다시 발급한 accessToken을 쿠키에 담는다.
+	 * 4. HttpServletResponse에 cookie를 담아서 보낸다.
+	 *
+	 * @param servletRequest
+	 * @param servletResponse
+	 * @param filterChain
+	 * @throws IOException
+	 * @throws ServletException
+	 */
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws
+		IOException,
+		ServletException {
+		HttpServletRequest httpServletRequest = (HttpServletRequest)servletRequest;
+		HttpServletResponse httpServletResponse = (HttpServletResponse)servletResponse;
 
-        String accessToken = "";
-        String refreshToken = "";
-        String userCode = "";
+		String requestURL = httpServletRequest.getServletPath();
+		if (requestURL.contains("swagger"))
+			filterChain.doFilter(httpServletRequest, httpServletResponse);
 
-        // Request에서 쿠키를 가져온 후 accessToken과 refreshToken을 추출
-        Cookie[] cookies = httpServletRequest.getCookies();
+		String accessToken = "";
+		String refreshToken = "";
+		String userCode = "";
 
-        //쿠키가 모두 만료되어 없거나
-        //하나의 토큰만 하나의 쿠키에 담겨오는 경우
-        //exception을 터뜨려 재로그인 하도록 해준다.
-        if(cookies == null || cookies.length == 1) {
-            removeAllCookies(httpServletResponse, cookies);
+		// Request에서 쿠키를 가져온 후 accessToken과 refreshToken을 추출
+		Cookie[] cookies = httpServletRequest.getCookies();
 
-            throw new UnAuthorizedException();
-        }
+		//쿠키가 모두 만료되어 없거나
+		//하나의 토큰만 하나의 쿠키에 담겨오는 경우
+		//exception을 터뜨려 재로그인 하도록 해준다.
+		if (cookies == null || cookies.length == 1) {
+			removeAllCookies(httpServletResponse, cookies);
 
-        for(Cookie cookie : cookies){
-            String tokenName = cookie.getName();
-            String tokenValue = cookie.getValue();
+			throw new UnAuthorizedException();
+		}
 
-            if(tokenName.equals("accessToken")){
-                accessToken = tokenValue;
+		for (Cookie cookie : cookies) {
+			String tokenName = cookie.getName();
+			String tokenValue = cookie.getValue();
 
-                //accessToken을 통해서 userCode를 가져온다
-                userCode = jwtProvider.getUserCode(accessToken);
-            } else if(tokenName.equals("refreshToken")){
-                refreshToken = tokenValue;
-            }
-        }
+			if (tokenName.equals("accessToken")) {
+				accessToken = tokenValue;
 
-        // 로그아웃 해서 만료된 refresh token을 가지고 접근 할 경우
-        // exception 터뜨림
-        jwtProvider.validateLogoutToken(refreshToken);
+				//accessToken을 통해서 userCode를 가져온다
+				userCode = jwtProvider.getUserCode(accessToken);
+			} else if (tokenName.equals("refreshToken")) {
+				refreshToken = tokenValue;
+			}
+		}
 
-        Map<String, Object> tokens = jwtProvider.reIssueToken(accessToken, refreshToken, userCode);
+		// 로그아웃 해서 만료된 refresh token을 가지고 접근 할 경우
+		// exception 터뜨림
+		jwtProvider.validateLogoutToken(refreshToken);
 
-        //만료된 토큰이 존재한다면
-        if(tokens != null){
-            Cookie accessTokenCookie = null;
-            Cookie refreshTokenCookie = null;
+		Map<String, Object> tokens = jwtProvider.reIssueToken(accessToken, refreshToken, userCode);
 
-            //access token을 보내줬다면
-            //access token이 만료 되었다는 의미
-            if(tokens.get("accessToken") != null){
-                //기존 accessToken을 담고 있던 쿠키 제거
-                removeCookie(httpServletResponse, cookies, "accessToken");
+		//만료된 토큰이 존재한다면
+		if (tokens != null) {
+			Cookie accessTokenCookie = null;
+			Cookie refreshTokenCookie = null;
 
-                //발급된 accessToken을 가져온다
-                TokenDTO accessTokenDTO = (TokenDTO) tokens.get("accessToken");
+			//access token을 보내줬다면
+			//access token이 만료 되었다는 의미
+			if (tokens.get("accessToken") != null) {
+				//기존 accessToken을 담고 있던 쿠키 제거
+				removeCookie(httpServletResponse, cookies, "accessToken");
 
-                accessTokenCookie = new Cookie("accessToken", accessTokenDTO.getToken());
+				//발급된 accessToken을 가져온다
+				TokenDTO accessTokenDTO = (TokenDTO)tokens.get("accessToken");
 
-                accessTokenCookie.setMaxAge(60 * 60 * 2);
-                accessTokenCookie.setPath("/");
-                accessTokenCookie.setHttpOnly(true);
-                accessTokenCookie.setSecure(false);
-            }
-            //refresh token을 보내줬다면
-            //refresh token이 만료 되었다는 의미
-            if(tokens.get("refreshToken") != null){
-                //기존 refreshToken을 담고 있던 쿠키 제거
-                removeCookie(httpServletResponse, cookies, "refreshToken");
+				accessTokenCookie = new Cookie("accessToken", accessTokenDTO.getToken());
 
-                TokenDTO refreshTokenDTO = (TokenDTO) tokens.get("refreshToken");
+				accessTokenCookie.setMaxAge(60 * 60 * 2);
+				accessTokenCookie.setPath("/");
+				accessTokenCookie.setHttpOnly(true);
+				accessTokenCookie.setSecure(false);
+			}
+			//refresh token을 보내줬다면
+			//refresh token이 만료 되었다는 의미
+			if (tokens.get("refreshToken") != null) {
+				//기존 refreshToken을 담고 있던 쿠키 제거
+				removeCookie(httpServletResponse, cookies, "refreshToken");
 
-                refreshTokenCookie = new Cookie("refreshToken", refreshTokenDTO.getToken());
+				TokenDTO refreshTokenDTO = (TokenDTO)tokens.get("refreshToken");
 
-                refreshTokenCookie.setMaxAge(60 * 24 * 24 * 31);
-                refreshTokenCookie.setPath("/");
-                refreshTokenCookie.setHttpOnly(true);
-                refreshTokenCookie.setSecure(false);
-            }
+				refreshTokenCookie = new Cookie("refreshToken", refreshTokenDTO.getToken());
 
-            // accessToken만 만료 되어서
-            // accessToken만 재발급
-            if(accessTokenCookie != null && refreshTokenCookie == null){
-                httpServletResponse.addCookie(accessTokenCookie);
-            }
-            // refreshToken만 만료 되어서
-            // refreshToken만 재발급
-            else if(accessTokenCookie == null && refreshTokenCookie != null){
-                httpServletResponse.addCookie(refreshTokenCookie);
-                refreshTokenRepository.save(RefreshToken.builder().refreshToken(refreshTokenCookie.getValue()).build());
-            }
-            //accessToken, refreshToken 모두 만료 되었을 시에
-            //재로그인 하라는 에러메시지를 보낸다
-            else if(accessTokenCookie != null && refreshTokenCookie != null){
-                throw new UnAuthorizedException();
-            }
-        }
+				refreshTokenCookie.setMaxAge(60 * 24 * 24 * 31);
+				refreshTokenCookie.setPath("/");
+				refreshTokenCookie.setHttpOnly(true);
+				refreshTokenCookie.setSecure(false);
+			}
 
-        filterChain.doFilter(httpServletRequest, httpServletResponse);
-    }
+			// accessToken만 만료 되어서
+			// accessToken만 재발급
+			if (accessTokenCookie != null && refreshTokenCookie == null) {
+				httpServletResponse.addCookie(accessTokenCookie);
+			}
+			// refreshToken만 만료 되어서
+			// refreshToken만 재발급
+			else if (accessTokenCookie == null && refreshTokenCookie != null) {
+				httpServletResponse.addCookie(refreshTokenCookie);
+				refreshTokenRepository.save(RefreshToken.builder().refreshToken(refreshTokenCookie.getValue()).build());
+			}
+			//accessToken, refreshToken 모두 만료 되었을 시에
+			//재로그인 하라는 에러메시지를 보낸다
+			else if (accessTokenCookie != null && refreshTokenCookie != null) {
+				throw new UnAuthorizedException();
+			}
+		}
 
-    //기존에 쿠키를 제거하는 로직
-    public void removeCookie(HttpServletResponse httpServletResponse, Cookie[] cookies, String key){
-        if(cookies != null){
-            for(Cookie cookie : cookies){
-                if(cookie.getName().equals(key)){
-                    cookie.setMaxAge(0);
-                    httpServletResponse.addCookie(cookie);
-                }
-            }
-        }
-    }
+		filterChain.doFilter(httpServletRequest, httpServletResponse);
+	}
 
-    //모든 쿠키를 제거하는 로직
-    public void removeAllCookies(HttpServletResponse httpServletResponse, Cookie[] cookies){
-        if(cookies != null){
-            for(Cookie cookie : cookies){
-                cookie.setMaxAge(0);
-                httpServletResponse.addCookie(cookie);
-            }
-        }
-    }
+	//기존에 쿠키를 제거하는 로직
+	public void removeCookie(HttpServletResponse httpServletResponse, Cookie[] cookies, String key) {
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals(key)) {
+					cookie.setMaxAge(0);
+					httpServletResponse.addCookie(cookie);
+				}
+			}
+		}
+	}
+
+	//모든 쿠키를 제거하는 로직
+	public void removeAllCookies(HttpServletResponse httpServletResponse, Cookie[] cookies) {
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				cookie.setMaxAge(0);
+				httpServletResponse.addCookie(cookie);
+			}
+		}
+	}
 }
