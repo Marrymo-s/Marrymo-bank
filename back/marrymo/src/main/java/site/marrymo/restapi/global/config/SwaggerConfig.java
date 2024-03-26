870de93f34100e33cc35456fe61c43a2dc559149
김재윤 <jaeyun1723@naver.com>
2024-03-26 09:06:02 +0900

    fix : swagger server

--- back/marrymo/src/main/java/site/marrymo/restapi/global/config/SwaggerConfig.java
+++ back/marrymo/src/main/java/site/marrymo/restapi/global/config/SwaggerConfig.java
@@ -1,8 +1,9 @@
 package site.marrymo.restapi.global.config;
 
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -10,14 +11,17 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @Configuration
 public class SwaggerConfig {
-    @Bean
-    public OpenAPI openAPI() {
-        Info info = new Info()
-                .title("marrymo API")
-                .version("1.0")
-                .description("marrymo APIs");
-        return new OpenAPI()
-                .info(info);
-    }
+	@Bean
+	public OpenAPI openAPI() {
+		Info info = new Info()
+			.title("marrymo API")
+			.version("1.0")
+			.description("marrymo APIs");
+		Server server = new Server();
+		server.setUrl("https://marrymo.site/api");
+		return new OpenAPI()
+			.info(info)
+			.addServersItem(server);
+	}
 }
 
