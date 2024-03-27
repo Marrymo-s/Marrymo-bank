646d6621a1c48a564fd1e008c8e8edc11c1c8c72
김재윤 <jaeyun1723@naver.com>
2024-03-27 17:44:55 +0900

    fix : scheduler

--- back/marrymo/src/main/java/site/marrymo/restapi/global/scheduler/config/SchedulerConfig.java
+++ back/marrymo/src/main/java/site/marrymo/restapi/global/scheduler/config/SchedulerConfig.java
@@ -25,8 +25,7 @@ public class SchedulerConfig {
 	private final ExcelService excelService;
 	private final SmtpService smtpService;
 
-//	@Scheduled(cron = "00 00 03 * * *", zone = "Asia/Seoul")
-	@Scheduled(cron = "00 34 17 * * *", zone = "Asia/Seoul")
+	@Scheduled(cron = "00 00 3 * * *", zone = "Asia/Seoul")
 	public void makeExcel() throws IOException, MessagingException {
 		List<User> users = cardService.findUserSequenceByWeddingDateAndIsIssued();
 		for (User user : users) {
