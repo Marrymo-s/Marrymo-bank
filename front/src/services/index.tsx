def616f6361f2a9606efcf089cdcb7e0594b00e8
정지원 <imjjw1004@naver.com>
2024-03-28 14:35:23 +0900

    feat : fetch baseurl, headers, credentials 추가 커스텀

--- front/src/services/index.tsx
+++ front/src/services/index.tsx
@@ -1,26 +1,18 @@
-import axios from 'axios';
-// 리액트 문법이 아니라서 .ts로 바꿔야함
-export const axiosInstance = axios.create({
-  baseURL: 'https://marrymo.site/api',
-  withCredentials: true,
-});
+export const fetchInstance = async (url: string, options: RequestInit = {}) => {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
 
-// 이미지 같은건 multipart라서 인터셉터는 처음에 한번 정의되고 다른데서 헤더를 고치면 지속돼서 상황별로 axios인스턴스를 여러개 만들어서 해야할수도 잇다
+  //header, credentials
+  const defaultOptions: RequestInit = {
+    ...options,
+    headers: {
+      'Content-Type': 'application/json',
+      ...options.headers,
+    },
+    credentials: 'include',
+  }
 
-// 요청 인터셉터 추가
-// 헤더담기
-axiosInstance.interceptors.request.use(async (config) => {
+  const response = await fetch(`${baseUrl}${url}`, defaultOptions)
 
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
-
-// 응답 인터셉터 추가
-// 에러처리 여기서
-//
-axiosInstance.interceptors.response.use((response) => {
-  return response;
-}, (error) => {
-  return Promise.reject(error);
-});
+  //응답 json으로 파싱
+  return response.json()
+}
