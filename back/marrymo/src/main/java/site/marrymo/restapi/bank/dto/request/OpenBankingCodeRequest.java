49dfc4027036cd5edf40974fe28a4807dea4c679
김하연 <khy3496@cau.ac.kr>
2024-03-29 23:51:51 +0900

    hotfix : Dto Valid annotation 추가

--- back/marrymo/src/main/java/site/marrymo/restapi/bank/dto/request/OpenBankingCodeRequest.java
+++ back/marrymo/src/main/java/site/marrymo/restapi/bank/dto/request/OpenBankingCodeRequest.java
@@ -1,5 +1,6 @@
 package site.marrymo.restapi.bank.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,9 @@ import site.marrymo.restapi.user.dto.Who;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OpenBankingCodeRequest {
+    @NotBlank( message = "누구(GROOM, BRIDE 택 1)의 통장인지 입력하세요")
     private Who who;
+    
+    @NotBlank( message = "open banking api로 받아온 코드를 입력해주세요")
     private String code;
 }
