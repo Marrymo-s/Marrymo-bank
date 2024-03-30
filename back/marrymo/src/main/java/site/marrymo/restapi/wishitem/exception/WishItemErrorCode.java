f56945647f0222d747c494f3740c720f81523531
박도연 <ehduszkdzkd@naver.com>
2024-03-31 08:11:56 +0900

    feat: wishitem 관련 error code 추가

--- back/marrymo/src/main/java/site/marrymo/restapi/wishitem/exception/WishItemErrorCode.java
+++ back/marrymo/src/main/java/site/marrymo/restapi/wishitem/exception/WishItemErrorCode.java
@@ -6,7 +6,10 @@ import site.marrymo.restapi.global.exception.ErrorCode;
 
 @Getter
 @AllArgsConstructor
-//public enum WishItemErrorCode implements ErrorCode {
-public enum WishItemErrorCode {
+public enum WishItemErrorCode implements ErrorCode{
+    WISH_ITEM_NOT_FOUNT_FOR_USER(404, "WIS_001", "USER를 통해 WISH ITEM을 찾을 수 없습니다.");
 
+    private int statusCode;
+    private String errorCode;
+    private String message;
 }
