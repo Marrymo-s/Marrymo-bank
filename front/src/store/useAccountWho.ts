337049954d2bd1459a131f03bbc1060c85c2f168
정지원 <imjjw1004@naver.com>
2024-04-01 15:38:42 +0900

    feat : useAccountWho 구현

--- front/src/store/useAccountWho.ts
+++ front/src/store/useAccountWho.ts
@@ -4,7 +4,9 @@ import { devtools, persist, createJSONStorage } from 'zustand/middleware'
 
 interface AccountWhoState {
   who: 'GROOM' | 'BRIDE' | 'BOTH' | null;
+  authStatus: {GROOM ?: boolean, BRIDE ?: boolean};
   setWho: (who: 'GROOM' | 'BRIDE' | 'BOTH' | null) => void;
+  setAuthStatus: (role: 'GROOM' | 'BRIDE', status: boolean) => void;
 }
 
 export const usesAccountWhoStore = create<AccountWhoState>()(
@@ -12,7 +14,11 @@ export const usesAccountWhoStore = create<AccountWhoState>()(
     persist(
       (set) => ({
         who: null,
+        authStatus: {},
         setWho: (who) => set({ who }),
+        setAuthStatus: (role, status) => set((state) => ({
+          authStatus: {...state.authStatus, [role]: status}
+        })),
       }),
       {
         name: 'account-who',
