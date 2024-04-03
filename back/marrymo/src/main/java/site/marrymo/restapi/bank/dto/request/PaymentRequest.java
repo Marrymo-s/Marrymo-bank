b742c1a0deffd9ce55a2c018fefcfe09552c1a09
김재윤 <jaeyun1723@naver.com>
2024-04-03 20:05:53 +0900

    fix : string

--- back/marrymo/src/main/java/site/marrymo/restapi/bank/dto/request/PaymentRequest.java
+++ back/marrymo/src/main/java/site/marrymo/restapi/bank/dto/request/PaymentRequest.java
@@ -10,9 +10,9 @@ public class PaymentRequest {
 	private String partner_order_id;
 	private String partner_user_id;
 	private String item_name;
-	private int quantity;
-	private int total_amount;
-	private int tax_free_amount;
+	private String quantity;
+	private String total_amount;
+	private String tax_free_amount;
 	private String approval_url;
 	private String cancel_url;
 	private String fail_url;
