c823680f348154df99e33b61c6e31634e61d8817
seunggwang <sg.ryu23@gmail.com>
2024-04-03 21:38:49 +0900

    fix: build 오류 수정

--- front/src/containers/home/index.tsx
+++ front/src/containers/home/index.tsx
@@ -21,18 +21,14 @@ type Props = {
 const Home = ({params}: Props) => {
   const { userCode } = useParams() as { userCode:string }
   const setUserCode = userInfoStore((state) => state.setUserCode);
-  const [isMem, setIsMem] = useState<boolean>(true); // 상태 추가
+  const [isMem, setIsMem] = useState<boolean>(false); // 상태 추가
 
   useEffect(() => {
     if (userCode) {
       setUserCode(userCode);
       console.log(userCode)
     }
-  }, [userCode, setUserCode, isMem]);
-
-  const updateIsMem = () => {
-    setIsMem(!isMem);
-  };
+  }, [userCode, setUserCode]);
   console.log(isMem)
   return (
     <>
@@ -40,7 +36,7 @@ const Home = ({params}: Props) => {
         {isMem && <HamburgerButton />} {/* 조건부 렌더링 */}
         <div className={styles.invitationContainer}>
           {/*<InvitationCard info={info}/>*/}
-          <InvitationCard params={{userCode}} updateIsMem={updateIsMem}/>
+          <InvitationCard params={{userCode}} setIsMem={setIsMem}/>
         </div>
       </main>
     </>
